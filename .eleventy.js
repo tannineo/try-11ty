93100504cdc27c10c4d663681a71bdbffc30695c
tannineo <tannineo@outlook.com>
2023-07-20 10:16:33 +0100

    njk templating & partials

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,14 @@ module.exports = function (eleventyConfig) {
   // Return your Object options:
   return {
     dir: {
-      input: 'content',
+      input: 'src',
+      data: '_data',
+      includes: '_includes',
+      layouts: '_layouts',
       output: 'dist',
     },
+    templateFormats: ['html', 'njk', 'md'],
+    markdownTemplateEngine: 'njk',
     pathPrefix: '/try-11ty/',
   }
 }
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+layout: base
+title: Home
+---
+
+# Hello World with 11ty!
+
+This is a site built with [Eleventy](https://www.11ty.io/).
